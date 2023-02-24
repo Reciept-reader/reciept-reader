d6b8d8b806b5a6e207b83575817529f60fcca334
Julio S Espinola <52016566+JulioSEspinola@users.noreply.github.com>
2023-02-23 22:43:29 -0800

    Updated About page

--- Mobile App/src/screens/About.js
+++ Mobile App/src/screens/About.js
@@ -4,34 +4,98 @@ import { Avatar } from 'react-native-elements';
 import { Image, StyleSheet, useWindowDimensions} from 'react-native';
 
 function About() {
-  const {height} = useWindowDimensions();
+
+  const staffMembers = [
+    {
+      name: 'Brian',
+      image: require('../../assets1/Brian.png'),
+    },
+    {
+      name: 'Ibrahim',
+      image: require('../../assets1/Ibrahim.png'),
+    },
+    {
+      name: 'Jacob',
+      image: require('../../assets1/Jacob.png'),
+    },
+    {
+      name: 'Ichinnorov',
+      image: require('../../assets1/Ichinnorov.png'),
+    },
+    {
+      name: 'Jatinder',
+      image: require('../../assets1/Jatinder.png'),
+    },
+    {
+      name: 'Julio',
+      image: require('../../assets1/Julio.png'),
+    },
+    {
+      name: 'Mason',
+      image: require('../../assets1/Mason.png'),
+    },
+    {
+      name: 'Nathanael',
+      image: require('../../assets1/Nathanael.png'),
+    },
+    {
+      name: 'William',
+      image: require('../../assets1/William.png'),
+    },
+  ];
+
     return (
-      <View style={{ flex: 1, alignItems: 'left' }}>
-        {/* <Text>CWU studnets -Project Receipt Reader</Text> */}
-        <Avatar
-  rounded
- 
-  style={[styles.logo, {height : height * 0.3}]}
-/>
-
-
-
-
-
+      <View style={styles.container}>
+      <Text style={styles.content}>CWU - Reciept Reader Project</Text>
+      <Text style={styles.title}>Meet Our Team</Text>
+      <View style={styles.staffContainer}>
+        {staffMembers.map((staffMember, index) => (
+          <View key={index} style={styles.staffItem}>
+            <Image style={styles.staffImage} source={staffMember.image} />
+            <Text style={styles.staffName}>{staffMember.name}</Text>
+          </View>
+        ))}
+      </View>
+        
       </View>
     );
   }
   const styles = StyleSheet.create( {
-    root: {
-       alignItems: 'center',
-       padding:20,
-        paddingBottom:40,
-       
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      padding: 10,
     },
-    logo: {
-        width: "70%",
-        maxWidth: 300,
-        maxHeight: 200,
+    title: {
+      fontSize: 20,
+      fontWeight: 'bold',
+      marginBottom: 20,
+    },
+    staffContainer: {
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+      justifyContent: 'center',
+    },
+    staffItem: {
+      alignItems: 'center',
+      margin: 10,
+    },
+    staffImage: {
+      width: 100,
+      height: 100,
+      borderRadius: 50,
+      marginBottom: 5,
+    },
+    staffName: {
+      fontSize: 16,
+      fontWeight: 'bold',
+      textAlign: 'center',
+    },
+    content:{
+      fontSize: 20,
+      marginBottom: 20,
+
     },
 });
 export default About;
