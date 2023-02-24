102590d6e9cb6f3ac33fb479515b50a038541cae
Jacob <jacobfisher311@gmail.com>
2023-02-24 09:20:30 -0800

    updated EditToDB to properly display the page

--- Mobile App/src/screens/EditToDB.js
+++ Mobile App/src/screens/EditToDB.js
@@ -1,27 +1,27 @@
 import { Text, View, StyleSheet, Button } from 'react-native';
 import React, {useState} from 'react';
 
-function postdata() {
-    const receiptData = {
-        user_id: '483aeb03-4566-459c-9035-5553483a22ef',
-        store_name: 'Safeway',
-        date: '11/11/1111',
-        total: '100',
-        items: [
+const receiptData = {
+    user_id: '483aeb03-4566-459c-9035-5553483a22ef',
+    store_name: 'Safeway',
+    date: '11/11/1111',
+    total: '100',
+    items: [
 
-            {item_name: 'pizza', price: 99},
-            {item_name: 'nerd', price: 88},
-            {item_name: 'more food', price: 77}
-        ]
-    };
+        {item_name: 'pizza', price: 99},
+        {item_name: 'nerd', price: 88},
+        {item_name: 'more food', price: 77}
+    ]
+};
 
+function postdata(input) {
     const options = {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
             'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml4eHRtaGp6dGxmc2Zqb3J1cmZpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzUyOTczNTcsImV4cCI6MTk5MDg3MzM1N30.o7s0gThwzgZ7OdeskinJc5Fz9A95fuUek22E1isUoYE',
         },
-        body: JSON.stringify(receiptData),
+        body: JSON.stringify(input),
     };
 
     fetch('https://ixxtmhjztlfsfjorurfi.functions.supabase.co/reciept', options);
@@ -37,9 +37,8 @@ function EditToDB() {
         
         <Text>Store: {receiptData.store}</Text>
         <Text>Date: {receiptData.date}</Text>
-        {receiptData.items.map(i => i.map(j => (<Text key={j}>{j}</Text>)))}
         <Text>Total: {receiptData.total}</Text>
-        <Button onPress={postdata} title="Click me!"></Button>
+        <Button onPress={postdata(receiptData)} title="Click me!"></Button>
       </View>
     );
 }
