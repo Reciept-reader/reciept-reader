6b0ab8449293d1a35a763929c405f37b043caa79
Nathnael7 <tearenati0@gmail.com>
2023-02-23 16:43:04 -0800

    Update

--- Mobile App/src/screens/CameraScreen.js
+++ Mobile App/src/screens/CameraScreen.js
@@ -38,7 +38,15 @@ export default function App() {
     let newPhoto = await cameraRef.current.takePictureAsync(options);
     setPhoto(newPhoto);
   };
-
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+  }
   if (photo) {
     
 
