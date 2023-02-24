7a8c117036774515eea8d9846a53327be30f4c4a
Sushi-Roll <120627190+Sushi-Roll-17@users.noreply.github.com>
2023-02-23 21:00:36 -0800

    Added user account functions

---
+++ Mobile App/src/userfunctions/loginFunctions.js
@@ -0,0 +1,98 @@
+import { createClient } from "@supabase/supabase-js";
+import dotenv from 'dotenv';
+dotenv.config();
+
+/* Sign up function
+Takes in a username and password to sign up 
+If the username doesnt exist works 
+If the username already exists 
+*/
+
+
+/* Sign in function
+Takes in a user entered username and password 
+Checks to see if the combination exists 
+If so returns the user_id and logs the user in 
+If false returns "login credenials dont exist" for the app to tell the user
+*/
+export async function signIn(username, password) {
+  const supabase = createClient(process.env.SUPA_URL, process.env.SUPA_ANON);
+  const { data: user, error } = await supabase
+    .from("users")
+    .select("user_id")
+    .eq("username", username)
+    .eq("password", password)
+
+  if (error) {
+    if (error.message.includes("duplicate key value violates unique constraint")) {
+        return "username taken";
+    } else {
+        throw error
+    }
+  }
+
+  if (user.user_id === undefined) {
+    return "login credentials don't exist";
+  }
+
+  return { user_id: user.user_id, username: user.username };
+};
+
+
+/* Password change function
+Takes in a user_id, users old password, and a new password
+Checks to see if the user_id and password combination exists in the db
+If so changes the password and returns success 
+If doesnt exist returns current password doesnt exist
+*/
+export async function changePassword(user_id, password, new_password) {
+    const supabase = createClient(process.env.SUPA_URL, process.env.SUPA_ANON);
+    const { data, error } = await supabase
+        .from("users")
+        .update({password: new_password})
+        .eq('user_id', user_id)
+        .eq('password', password)
+        .select()
+
+    if (error) {
+        console.error(error);
+        return "password is incorrect";
+    }
+
+    if (data.length == 0) {
+        return "password is incorrect";
+    }
+    
+    return 'Password changed';
+}
+
+
+  /* Username change function
+Takes in a user_id, users old username, and a new username
+Checks to see if the user_id and username combination exists in the db
+If so changes the username and returns success 
+If doesnt exist returns current username doesnt exist
+*/
+export async function changeUsername(user_id, username, new_username) {
+    const supabase = createClient(process.env.SUPA_URL, process.env.SUPA_ANON);
+    const { data, error } = await supabase
+        .from("users")
+        .update({username: new_username})
+        .eq('user_id', user_id)
+        .eq('username', username)
+        .select()
+
+    if (error) {
+        console.error(error);
+        return "username is incorrect";
+
+    }
+
+    if (data.length == 0) {
+        return "username is incorrect";
+    }
+    
+    return 'Username changed';
+};
+
+export default { signIn , signUp, changePassword, changeUsername }
