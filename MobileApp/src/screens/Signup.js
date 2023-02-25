73bf8cafc41b7ff52cd4dc649100021d891a1b37
Nathnael7 <tearenati0@gmail.com>
2023-02-24 16:14:17 -0800

    NEW

--- MobileApp/src/screens/Signup.js
+++ MobileApp/src/screens/Signup.js
@@ -34,6 +34,12 @@ function Signup() {
             value={username}
             setValue={setUsername}
         />
+         <CustomInput
+            placeholder="Useremail"
+
+            value={username}
+            setValue={setUsername}
+        />
         <CustomInput
             placeholder="Create password"
 
