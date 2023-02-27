e05e020378bfdb2b2110f0acaedfbe5ac8fbb54d
Julio S Espinola <52016566+JulioSEspinola@users.noreply.github.com>
2023-02-27 02:24:48 -0800

    Display reciept count in profile

--- MobileApp/src/screens/ProfileScreen.js
+++ MobileApp/src/screens/ProfileScreen.js
@@ -3,26 +3,43 @@ import { Avatar } from 'react-native-elements';
 import Logo from '../../assets1/logo2.png';
 import CustomButton from '../components/CustomButton/CustomButton';
 import { useNavigation } from '@react-navigation/native';
+import React, { useState, useEffect } from 'react';
+import { usernameAndCount } from '../functions/userDataFun';
 
-function ProfileScreen({ route, navigation }) {
+function ProfileScreen({ route, navigation, props }) {
+
+    
 
     // const navigation = useNavigation();
     const userParams = route.params;
     alert(userParams.userid)
+    
+    const [username, setUsername] = useState("");
+    const [recieptCount, setRecieptCount] = useState(0);
+
+    useEffect(() => {
+        async function fetchData() {
+            const {username, recieptCount} = await usernameAndCount(params.userid);
+            setUsername(username);
+            setRecieptCount(recieptCount);
+        }
+        fetchData();
+    }, []);
+
     const onSignOutPressed = () => {
         navigation.replace('Sign In')
         };
 
-        const onAboutPressed = () => {
-            navigation.navigate('About')
-            };
+    const onAboutPressed = () => {
+        navigation.navigate('About')
+        };
     return (
 
         <View style={style.container}>
             <View style={style.infoContainer}>
                 <Text style={style.header} >Welcome</Text>
                 <Text style={style.userInfo}>User Name: {(userParams.userid).toString()}</Text>
-                <Text style={style.userInfo}>Reciept Amount:</Text>
+                <Text style={style.userInfo}>Reciept Amount: {recieptCount} </Text>
             </View>
             
             <View style = {style.buttonsContainer}>
