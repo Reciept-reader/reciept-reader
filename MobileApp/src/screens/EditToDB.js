ec87a20df357a241fe4a1d70923f27964b79aec1
Jacob <jacobfisher311@gmail.com>
2023-02-27 18:10:59 -0800

    updated packages, edittodb deletes elements

--- MobileApp/src/screens/EditToDB.js
+++ MobileApp/src/screens/EditToDB.js
@@ -1,4 +1,4 @@
-import { Text, View, StyleSheet, ScrollView, TextInput } from 'react-native';
+import { Text, View, StyleSheet, ScrollView, TextInput, Button } from 'react-native';
 import React, {useState} from 'react';
 import CustomButton from '../components/CustomButton/CustomButton';
 
@@ -6,40 +6,6 @@ const EditToDB = ({ route, navigation }) => {
     const receiptData = route.params.receiptData;
     const userid = route.params.userid;
     const [itemContent, setItemContent] = useState(receiptData.items)
-
-    const updateReceipt = () => {
-        let s_store = store_nameDB
-        let s_total = totalDB
-        if (s_store.trim() == '') s_store = receiptData.store_name
-        if (s_total.trim() == '') s_total = receiptData.total
-
-        const s_ReceiptData = {
-            user_id: userid,
-            store_name: s_store,
-            date: '',
-            total: s_total,
-            items: itemContent
-        }
-        return s_ReceiptData
-    };
-
-    const postdata = (input) => { 
-        // const options = {
-        //     method: 'POST',
-        //     headers: {
-        //         'Content-Type': 'application/json',
-        //         'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml4eHRtaGp6dGxmc2Zqb3J1cmZpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzUyOTczNTcsImV4cCI6MTk5MDg3MzM1N30.o7s0gThwzgZ7OdeskinJc5Fz9A95fuUek22E1isUoYE',
-        //     },
-        //     body: JSON.stringify(input),
-        // };
-    
-        // fetch('https://ixxtmhjztlfsfjorurfi.functions.supabase.co/reciept', options);
-        for (let i in itemContent) {
-            alert(`item: ${itemContent[i].item_name} price: ${itemContent[i].price}`)
-        }
-        navigation.replace('HomeScreen', {userid: userid})
-    }
-    
     const [store_nameDB, onChangeName] = useState('')
     const [dateDB, onChangeDate] = useState('')
     const [totalDB, onChangeTotal] = useState('')
@@ -54,6 +20,49 @@ const EditToDB = ({ route, navigation }) => {
         const newItemContent = [...itemContent]
         newItemContent[index].price = text
         setItemContent(newItemContent)
+    }
+    
+    const removeInput = (index) => {
+        let data = [...itemContent];
+        data.splice(index, 1)
+        setItemContent(data)
+    }
+    
+    const addInput = (index) => {
+        let newInput = {item_name: 'Item Name', price: 'Price'}
+        return [...itemContent, newInput]
+    }
+    const updateReceipt = () => {
+        let s_store = store_nameDB
+        let s_total = totalDB
+        if (s_store.trim() == '') s_store = receiptData.store_name
+        if (s_total.trim() == '') s_total = receiptData.total
+
+        const s_ReceiptData = {
+            user_id: userid,
+            store_name: s_store,
+            date: '',
+            total: s_total,
+            items: undefined
+        }
+        return s_ReceiptData
+    };
+
+    const postdata = (input) => { 
+        // const options = {
+        //     method: 'POST',
+        //     headers: {
+        //         'Content-Type': 'application/json',
+        //         'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml4eHRtaGp6dGxmc2Zqb3J1cmZpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzUyOTczNTcsImV4cCI6MTk5MDg3MzM1N30.o7s0gThwzgZ7OdeskinJc5Fz9A95fuUek22E1isUoYE',
+        //     },
+        //     body: input,
+        // };
+    
+        // fetch('https://ixxtmhjztlfsfjorurfi.functions.supabase.co/reciept', options);
+        for (let i in itemContent) {
+            alert(`item: ${itemContent[i].item_name} price: ${itemContent[i].price}`)
+        }
+        navigation.replace('HomeScreen', {userid: userid})
     }
     
     if(receiptData.date == '') {
@@ -79,19 +88,20 @@ const EditToDB = ({ route, navigation }) => {
         onChangeText={onChangeDate}
         />
         
-        {receiptData.items.map((item, index) => 
+        {itemContent.map((item, index) => 
             <>
-            <TextInput
-            key={`item${item.id}`}
-            style={styles.input}
-            placeholder={item.item_name} 
-            onChangeText={text => changeItemContent(text, index)}
-            />
-            <TextInput key={`price${item.id}`}
-            style={styles.input}
-            placeholder={`${item.price}`} 
-            onChangeText={text => changePriceContent(text, index)}
-            />
+                <TextInput
+                key={`item${item.id}`}
+                style={styles.input}
+                placeholder={item.item_name} 
+                onChangeText={text => changeItemContent(text, index)}
+                />
+                <TextInput key={`price${item.id}`}
+                style={styles.input}
+                placeholder={`${item.price}`} 
+                onChangeText={text => changePriceContent(text, index)}
+                />
+                <Button title='Button' onPress={removeInput(index)}>Remove</Button>
             </>
         )} 
 
