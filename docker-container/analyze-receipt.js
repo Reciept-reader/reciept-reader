e1c3ca3c63ade9250f11e222b120575e713321dc
Ichinnorov Tuguldur <ichinnorov@nestsolutions.llc>
2023-03-01 19:42:40 -0800

    tesseract error

--- docker-container/analyze-receipt.js
+++ docker-container/analyze-receipt.js
@@ -1,28 +1,32 @@
 import { createWorker } from 'tesseract.js';
 
 async function analyze_receipt(input) {
-  
-    const worker = await createWorker();
-    await worker.loadLanguage('eng');
-    await worker.initialize('eng');
-    /**
-     * Creation of Line data under the variable "lines" 
-     * Line data consists of a collection of words, and
-     * corresponding confidence. Currently this function
-     * creates a 2D array "arr" that contains line data,
-     * and the corresponding confidence for the entire 
-     * line. 
-    **/
-    const { data: { lines } } = await worker.recognize(input);
-    
-    let arr = new Array(lines.length);
-    
-    for (let i = 0; i < lines.length; i++) {
-      //arr[i] = [lines[i].text, lines[i].confidence];
-      arr[i] = lines[i].text
+
+    try{
+      const worker = await createWorker();
+      await worker.loadLanguage('eng');
+      await worker.initialize('eng');
+      /**
+       * Creation of Line data under the variable "lines" 
+       * Line data consists of a collection of words, and
+       * corresponding confidence. Currently this function
+       * creates a 2D array "arr" that contains line data,
+       * and the corresponding confidence for the entire 
+       * line. 
+      **/
+      const { data: { lines } } = await worker.recognize(input);
+      
+      let arr = new Array(lines.length);
+      
+      for (let i = 0; i < lines.length; i++) {
+        //arr[i] = [lines[i].text, lines[i].confidence];
+        arr[i] = lines[i].text
+      }
+      await worker.terminate();
+      return arr;
+    }catch(error){
+      console.log("error occured")
     }
-    await worker.terminate();
-    return arr;
   };
 
   export default analyze_receipt;
