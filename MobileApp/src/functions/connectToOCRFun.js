effbf5ac7a4fd9abea7c92d6f0e6e07961709bfd
sharktrexer <sharktrexer@gmail.com>
2023-03-11 08:59:31 -0800

    final comments and var name changes (onlu container related)

--- MobileApp/src/functions/connectToOCRFun.js
+++ MobileApp/src/functions/connectToOCRFun.js
@@ -1,11 +1,11 @@
 import fetch from 'node-fetch';
 
 //What is assumed to always be where the OCR container is hosted
-const linkToAWSEC2Server = 'http://ec2-35-89-83-59.us-west-2.compute.amazonaws.com:3000/';
+const linkToContainer = 'http://ec2-35-89-83-59.us-west-2.compute.amazonaws.com:3000/';
 
-//host URL can be changed by passing a new parameter if desired
-//image URL is the path to a publically host image for the OCR to analyze
-export async function getDataFromOCR(imageUrl, hostUrl = linkToAWSEC2Server) { 
+//host URL can be changed by passing a new parameter in CameraScreen.js, line 150, if desired
+//image URL is the path to a publically hosted image for the OCR to analyze
+export async function getDataFromOCR(imageUrl, hostUrl = linkToContainer) { 
 
     //Connect to host
     const response = await fetch(hostUrl, {
